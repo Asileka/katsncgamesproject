90845dbb6cd639177f99f0b5469561b9b5ab67a8
Asileka <asilkatya@gmail.com>
2022-07-06 12:06:16 +0100

    removed extra SELECT query

--- models/ncgamesmodel.js
+++ models/ncgamesmodel.js
@@ -17,25 +17,18 @@ exports.fetchReviewByID = (reviewID) => {
       });
   }
 };
+
 exports.patchReview = (reviewID, newVote) => {
   let valuesArr = [];
   if (reviewID) {
     valuesArr.push(reviewID);
+    newVote = newVote.toString();
+    valuesArr.push(newVote);
     return connection
-      .query("SELECT * FROM reviews WHERE review_id = $1", valuesArr)
-      .then((results) => {
-        return results.rows[0];
-      })
-      .then((review) => {
-        let votes = review.votes;
-        votes += newVote;
-        votes = votes.toString();
-        valuesArr.push(votes);
-        return connection.query(
-          "UPDATE reviews SET votes = $2 WHERE review_id = $1 RETURNING *;",
-          valuesArr
-        );
-      })
+      .query(
+        "UPDATE reviews SET votes = votes + $2 WHERE review_id = $1 RETURNING *;",
+        valuesArr
+      )
       .then((newReview) => {
         return newReview.rows[0];
       })
