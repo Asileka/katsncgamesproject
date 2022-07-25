0ca39b50051f8c5e922f4203dd519e8014e38128
Asileka <asilkatya@gmail.com>
2022-07-25 15:32:58 +0100

    hosting

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,5 @@
 const ENV = process.env.NODE_ENV || "development";
 
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
 if (!process.env.PGDATABASE) {
   throw new Error("PGDATABASE not set");
 }
