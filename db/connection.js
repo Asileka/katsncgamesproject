1c8ca1e520e09ab4fdaaa4f8d2127bea951e697b
Asileka <asilkatya@gmail.com>
2022-07-25 15:36:25 +0100

    hosting

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,5 @@
 const ENV = process.env.NODE_ENV || "development";
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
-}
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
